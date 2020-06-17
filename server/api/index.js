6512080e778ea509c92c1a62464d210b3745bae1
Devin Knight <knightdevin@hotmail.com>
2020-06-16 21:17:23 -0400

    routes cleanup - entire json displaying in terminal. needs narrowed down

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,9 @@ module.exports = router
 
 router.use('/users', require('./users'))
 
+// schools mounted on api/schools
+router.use('/schools', require('./schools'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
--- server/api/schools.js
+++ server/api/schools.js
@@ -1,24 +1,46 @@
 const router = require('express').Router()
-const {dataGovKey} = require('../../secrets')
+import {dataGovKey, basicAuthCreds} from '../../secrets'
 // const fetch = require('node-fetch')
-module.exports = router
+export default router
 
-router.get('/schools', async (req, res) => {
-  // console.log('>>>>', req.params)
-  // let city = req.params.city
-  // let state = req.params.state
-  // let apiUrl = `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${dataGovKey}&location=${city}+${state}`
+// // initial attempt at api request....
+// router.get('/schools', async (req, res, next) => {
+//   try {
+//     // console.log('>>>>', req.params)
 
-  let apiUrl = `https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=${dataGovKey}`
+//     let apiUrl = `https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=${dataGovKey}`
 
-  let fetchResponse = await fetch(apiUrl)
-  const fetchedJson = await fetchResponse.json()
-  res.json(fetchedJson)
-  // } catch (error) {
-  //   next(error)
-  // }
+//     let fetchResponse = await fetch(apiUrl)
+//     const fetchedJson = await fetchResponse.json()
+//     res.json(fetchedJson)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+// note: -->  sending entire gov school json to terminal
+router.get('/', (req, res) => {
+  const schoolJSON = getSchoolsFromApi()
+  res.json(schoolJSON)
 })
 
+// code snipped created through postman
+function getSchoolsFromApi() {
+  var request = require('request')
+  var options = {
+    method: 'GET',
+    url: `https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=${dataGovKey}`,
+    headers: {
+      Authorization: basicAuthCreds
+    }
+  }
+  request(options, function(error, response) {
+    if (error) throw new Error(error)
+    console.log(response.body)
+  })
+}
+
+// ---------------------------------------- ALTERNATE THOUGHTS --------------------------------
 // fetch(
 //   "https://api.data.gov/ed/collegescorecard/v1/schools?api_key="
 //   +
@@ -35,36 +57,3 @@ router.get('/schools', async (req, res) => {
 //   console.error(err);
 //  resp.send(202);
 // });
-
-// // `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${dataGovKey}&location=Denver+CO`
-
-// async function getSchools() {
-//   let response = await fetch(
-//     `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${dataGovKey}&location=Denver+CO`
-//   )
-//   let data = await response.json
-//   return data
-// }
-
-// note: -->  sending school json to terminal
-router.get('/', async (req, res) => {
-  const schoolJSON = await getSchoolsFromApi()
-  res.json(schoolJSON)
-})
-
-// code snipped created through postman
-function getSchoolsFromApi() {
-  var request = require('request')
-  var options = {
-    method: 'GET',
-    url: `https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=${dataGovKey}`,
-    headers: {
-      Authorization:
-        'Basic YW55c3RyaW5nOlFHbmdaRE1nSldPTzNGeFA5N3pOMVpLTXdPUWQwcDRCeUVVNHhZZFg='
-    }
-  }
-  request(options, function(error, response) {
-    if (error) throw new Error(error)
-    console.log(response.body)
-  })
-}
