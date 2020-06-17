3b5b427b6bb42015ac5b6d7bc06b5d6ad8c1f688
Devin Knight <knightdevin@hotmail.com>
2020-06-16 20:17:48 -0400

    Revert "getting Data.GOV full json in terminal from  postman request. Need to narrow down"
    This reverts commit 7391dfa2bc1f1e69d186a4eff2d33831c6a9479b.

--- client/components/school-Info.js
+++ client/components/school-Info.js
@@ -1,5 +1,4 @@
 import React from 'react'
-const {dataGovKey} = require('../../secrets')
 
 export default function SchoolInfo() {
   return (
@@ -8,15 +7,6 @@ export default function SchoolInfo() {
       <ul>Name</ul>
       <ul>school website</ul>
       <ul>city (followed by stat and Zip code...etc.</ul>
-      <script>
-        {/* {
-        console.log('about to fetch data')
-        fetch(`https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${dataGovKey}&location=Denver+CO`).then(response => {
-          return response.json)
-        })
-      } */}
-        TEST
-      </script>
     </div>
   )
 }
--- server/api/index.js
+++ server/api/index.js
@@ -3,9 +3,6 @@ module.exports = router
 
 router.use('/users', require('./users'))
 
-// Schools from Data.Gov mounted on api/schools
-router.use('/schools', require('./schools'))
-
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
